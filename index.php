7deabf73730a01663f6fc2499904267524100bc6
chartchaike <chartchai.ke@pttgcgroup.com>
2014-02-04 14:44:48 +0700

    initiate state

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Index PHP</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
