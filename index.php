8d4a13fb82c640a18a615d0f6d2565c09fe783f1
chartchaike <chartchai.ke@pttgcgroup.com>
2014-02-04 15:24:44 +0700

    yes new branch

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Index - Githut PHP</title>
+        <title>Index - Githut Branching PHP</title>
     </head>
     <body>
         
